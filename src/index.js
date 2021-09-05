5be857fb056771a18960d20f22d4c8bd2a8a1d2e
arvagas <arvagas@gmail.com>
2021-09-04 23:52:41 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Prompt Calendar</h1>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
