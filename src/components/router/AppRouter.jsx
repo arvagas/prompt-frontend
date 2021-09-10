2118973dc70533a38a7bbce2297a244496e59d6d
arvagas <arvagas@gmail.com>
2021-09-10 15:10:34 -0400

    created user context to store login information

--- src/components/router/AppRouter.jsx
+++ src/components/router/AppRouter.jsx
@@ -3,13 +3,18 @@ import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 // component imports
 import Month from '../calendar/Month';
+// context imports
+import { UserContext } from '../../contexts/UserContext';
 
 const AppRouter = () => {
+  const [token, setToken] = useState(false);
 
   return (
-    <Switch>
-      <Route exact path="/" component={Month} />
-    </Switch>
+    <UserContext.Provider userValues={userValues}>
+      <Switch>
+        <Route exact path="/" component={Month} />
+      </Switch>
+    </UserContext.Provider>
   );
 };
 
