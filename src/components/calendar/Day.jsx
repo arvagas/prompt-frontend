b01c6ac9b97b088357924977f04995ba381c6bd1
arvagas <arvagas@gmail.com>
2021-09-10 14:39:59 -0400

    added more ways to close modal; escape wip

--- src/components/calendar/Day.jsx
+++ src/components/calendar/Day.jsx
@@ -1,5 +1,5 @@
 // library imports
-import React, {useState} from "react";
+import React from "react";
 // component imports
 import AppointmentModal from "../modals/AppointmentModal";
 // hook imports
